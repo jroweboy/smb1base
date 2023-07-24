0589778701ae1ae30280d91885050a4ea37e7b6a
James Rowe <jroweboy@gmail.com>
2023-07-24 18:55:39 -0400

    switch include sprites and bg location

--- src/chr.s
+++ src/chr.s
@@ -13,8 +13,8 @@
 ; .incbin "../Super Mario Bros. (World).nes", $9010, $1000
 ; .incbin "../Super Mario Bros. (World).nes", $8010, $1000
 
-.incbin "../chr/arranged_sprites.chr"
 .incbin "../chr/arranged_bg.chr"
+.incbin "../chr/arranged_sprites.chr"
 
 ; include the new title screen graphics
 .segment "CHR_TITLE_BG_0"
