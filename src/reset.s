d4ba1f71b7d99436d48cde893f8c1a35213340c2
NESDRAUG <127947615+NESDRAUG@users.noreply.github.com>
2023-08-01 02:53:08 +0200

    enemies palette in castle

--- src/reset.s
+++ src/reset.s
@@ -732,7 +732,7 @@ CastlePaletteData:
   .byte $0f, $30, $16, $00
   .byte $0f, $27, $17, $00
   .byte $0f, $16, $27, $18
-  .byte $0f, $1c, $36, $17
+  .byte $0f, $03, $30, $26 ; enemies 
   .byte $0f, $16, $30, $27
   .byte $0f, $00, $30, $10
   .byte $00
