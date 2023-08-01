df3d94f51f63b752323ed18c495eec5bbdc9f7dd
James Rowe <jroweboy@gmail.com>
2023-08-01 16:21:04 -0400

    Add cut and uncut versions of panic

---
+++ audio/panic_cut/superblocks_panic_cut.s
@@ -0,0 +1,19 @@
+.include "blocks_panic_cut.inc"
+
+.segment "HDRS"
+
+;.export sblk_header_s
+;.struct sblk_header_s
+;	bits_bank       .byte
+;	slopes_bank     .byte
+;	bits            .addr
+;	slopes          .addr
+;	initial_sample  .byte
+;	length          .byte
+;.endstruct
+
+.export sblk_table
+sblk_table:
+.export num_sblk_headers
+num_sblk_headers:
+	.byte 0
