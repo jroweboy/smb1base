485dcdb5acfccf0dea76c7dd07ba6e97c4d38003
James Rowe <jroweboy@gmail.com>
2025-02-10 14:34:00 -0500

    WIP update to the latest base code

---
+++ scripts/gen_tables.py
@@ -0,0 +1,75 @@
+import math
+import struct
+import pathlib
+
+basepath = pathlib.Path.cwd().joinpath("src/")
+
+rounding_mode = round
+
+
+def sling_curve():
+  maxmag = 52
+  invert = -1
+  maxspeed = 8
+  with open(basepath.joinpath("slingcurve.bin"), "wb") as sling:
+    with open(basepath.joinpath("slingfractional.bin"), "wb") as fract:
+      for mag in range(maxmag):
+        sling.write(struct.pack("=b",rounding_mode(math.sin(math.radians(mag * 90.0 / maxmag)) * maxspeed * invert)))
+        fractional = math.floor((math.sin(math.radians(mag * 90.0 / maxmag) * maxspeed) % 1) * 255.0)
+        # print(fractional)
+        fract.write(struct.pack("=B", fractional))
+
+
+def angular_momentum():
+  maxanglespeed = 32
+  with open(basepath.joinpath("rotation_speed.bin"), "wb") as speed:
+    for i in range(0,256):
+      as_signed = -1 * (256 - i) if i > 128 else i
+      val = as_signed * maxanglespeed / 128.0
+      speed.write(struct.pack("=b",rounding_mode(val)))
+
+
+def fireball_speed():
+  max_x_speed = 0x40
+  max_y_speed = 0x06
+  angles = 64
+  with open(basepath.joinpath("fireball_x_speed.bin"), "wb") as x:
+    with open(basepath.joinpath("fireball_y_speed.bin"), "wb") as y:
+      for angle in range(0,angles):
+        x_speed = max_x_speed * math.cos(math.radians(angle * 360.0 / angles))
+        y_speed = max_y_speed * math.sin(math.radians(angle * 360.0 / angles))
+        x.write(struct.pack("=b",rounding_mode(x_speed)))
+        y.write(struct.pack("=b",rounding_mode(y_speed)))
+
+
+def fulltable():
+  rounding_mode = round
+  invert = -1
+  maxdraw = 48
+  angles = 256
+  with open(basepath.joinpath("sinetable.bin"), "wb") as f:
+    for magnitude in range(maxdraw):
+      for angle in range(angles):
+        f.write(struct.pack("=b",rounding_mode(math.sin(math.radians(angle * 360.0 / angles)) * magnitude * invert)))
+  with open(basepath.joinpath("cosinetable.bin"), "wb") as f:
+    for magnitude in range(maxdraw):
+      for angle in range(angles):
+        f.write(struct.pack("=b",rounding_mode(math.cos(math.radians(angle * 360.0 / angles)) * magnitude * invert)))
+
+def maxtable():
+  invert = -1
+  maxdraw = 48
+  angles = 256
+  with open(basepath.joinpath("sinetable.bin"), "wb") as f:
+    for angle in range(angles):
+      f.write(struct.pack("=b",rounding_mode(math.sin(math.radians(angle * 360.0 / angles)) * maxdraw * invert)))
+  with open(basepath.joinpath("cosinetable.bin"), "wb") as f:
+    for angle in range(angles):
+      f.write(struct.pack("=b",rounding_mode(math.cos(math.radians(angle * 360.0 / angles)) * maxdraw * invert)))
+
+if __name__ == "__main__":
+  # fulltable()
+  sling_curve()
+  angular_momentum()
+  maxtable()
+  fireball_speed()
---
+++ scripts/rotate.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+from pilbmp2nes import pilbmp2chr
+
+SLINGSHOT_BOX = (0, 0, 24, 32)
+MARIO_BOX = (16, 0, 40, 24)
+HOLSTERED_BOX = (40, 0, 64, 24)
+BAND_BOX = (0, 24, 8, 32)
+FLAG1_BOX = (24, 24, 32, 32)
+FLAG2_BOX = (32, 24, 40, 32)
+FLAG3_BOX = (40, 24, 48, 32)
+
+MARIO_WIDE_BOX = (64, 0, 96, 32)
+MARIO_WIDE_HOLSTERED_BOX = (96, 0, 128, 32)
+
+def main():
+  fulldrawing = Image.open("../chr/raw/mario_angry.bmp")
+  slingshot = fulldrawing.crop(SLINGSHOT_BOX)
+  mario = fulldrawing.crop(MARIO_BOX)
+  holstered = fulldrawing.crop(HOLSTERED_BOX)
+  band = fulldrawing.crop(BAND_BOX)
+  flag1 = fulldrawing.crop(FLAG1_BOX)
+  flag2 = fulldrawing.crop(FLAG2_BOX)
+  flag3 = fulldrawing.crop(FLAG3_BOX)
+
+  angles = 64
+  direc = -1
+  for i in range(0,angles):
+    angle = direc * i * (360.0 / angles)
+    out = Image.new("P", (128, 32))
+    out.putpalette(fulldrawing.palette)
+    outbig = Image.new("P", (128, 32))
+    outbig.putpalette(fulldrawing.palette)
+    out.paste(slingshot, SLINGSHOT_BOX)
+    out.paste(mario.rotate(angle), MARIO_BOX)
+    out.paste(holstered.rotate(angle), HOLSTERED_BOX)
+    out.paste(mario.resize(size=(32,32)).rotate(angle), MARIO_WIDE_BOX)
+    out.paste(holstered.resize(size=(32,32)).rotate(angle), MARIO_WIDE_HOLSTERED_BOX)
+    out.paste(band.rotate(angle), BAND_BOX)
+    out.paste(flag1.rotate(angle), FLAG1_BOX)
+    out.paste(flag2.rotate(angle), FLAG2_BOX)
+    out.paste(flag3.rotate(angle), FLAG3_BOX)
+    out.save(f"../build/rotate_{i}.bmp")
+    b = pilbmp2chr(out, 8, 16)
+    with open(f"../chr/rotate/small_rotate_{i}.chr", 'wb') as f:
+      f.write(b''.join(b))
+    with open(f"../chr/rotate/big_rotate_{i}.chr", 'wb') as f:
+      f.write(b''.join(b))
+
+if __name__ == "__main__":
+  main()
